d3d4bc9fbdd117a40dd9c0ed84bbc87bed1c5086
hirotoyoshidome <kichiru.3161026@gmail.com>
2026-02-24 14:23:22 +0900

    fix test endpoint

--- src/tests.zig
+++ src/tests.zig
@@ -1,5 +1,6 @@
 // store tests
 const _store_hash = @import("store/object/hash.zig");
+const _store_constrained_types = @import("store/object/constrained_types.zig");
 const _store_atomic = @import("store/storage/atomic_write.zig");
 const _store_lock = @import("store/storage/lock.zig");
 const _store_time_utc = @import("store/storage/time/utc.zig");
@@ -15,6 +16,7 @@ const _ops_commit = @import("ops/commit_ops.zig");
 
 test "load modules" {
     _ = _store_hash;
+    _ = _store_constrained_types;
     _ = _store_atomic;
     _ = _store_lock;
     _ = _store_time_utc;
