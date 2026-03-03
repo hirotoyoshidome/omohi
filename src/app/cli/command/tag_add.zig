0e748513fb87735808e3a559dd3ead5f3e4f79d1
hirotoyoshidome <kichiru.3161026@gmail.com>
2026-03-03 10:08:07 +0900

    add commands of tags

---
+++ src/app/cli/command/tag_add.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+const parser_types = @import("../parser/types.zig");
+const command_types = @import("types.zig");
+const environment = @import("../environment.zig");
+const presenter = @import("../presenter/output.zig");
+const exit_code = @import("../exit_code.zig");
+const tag_ops = @import("../../../ops/tag_ops.zig");
+
+pub fn run(allocator: std.mem.Allocator, args: parser_types.TagAddArgs) !command_types.CommandResult {
+    var omohi = try environment.openOmohiDir(allocator, false);
+    defer omohi.deinit(allocator);
+
+    try tag_ops.add(allocator, omohi.dir, args.commit_id, args.tag_names);
+    const output = try presenter.message(allocator, "tags added\n");
+    return .{ .output = output, .to_stderr = false, .exit_code = exit_code.ok };
+}
