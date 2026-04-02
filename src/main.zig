1d46b7ba2a0826477b7f20bef81b5a182419c5f1
hirotoyoshidome <kichiru.3161026@gmail.com>
2026-04-02 13:49:55 +0900

    fix comments for entrypoiint

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const cli = @import("app/cli/run.zig");
 
+// Starts the CLI process and exits with the code returned by the application runner.
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
