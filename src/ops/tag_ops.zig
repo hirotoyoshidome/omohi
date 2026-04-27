584f03789622ea2ee9e8aa6604a2fe90d3bc1d80
hirotoyoshidome <kichiru.3161026@gmail.com>
2026-04-27 10:14:16 +0900

    feat: fixed tag ops to add all tag list
    added getting all tag list function for omohi tag command add.

--- src/ops/tag_ops.zig
+++ src/ops/tag_ops.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
 
 const store_api = @import("../store/api.zig");
+const test_support = @import("../testing/store_api_test_support.zig");
 
 pub const TagList = store_api.TagList;
+pub const TagNameList = store_api.StringList;
+
+/// Lists all known tag names.
+pub fn listAll(allocator: std.mem.Allocator, omohi_dir: std.fs.Dir) !TagNameList {
+    try store_api.ensureStoreVersion(allocator, omohi_dir);
+    return store_api.tagNameList(allocator, omohi_dir);
+}
 
 /// Lists tags for a commit.
 pub fn list(allocator: std.mem.Allocator, omohi_dir: std.fs.Dir, commit_id: []const u8) !TagList {
@@ -13,6 +21,11 @@ pub fn list(allocator: std.mem.Allocator, omohi_dir: std.fs.Dir, commit_id: []co
 // Releases the owned tag strings returned by tag queries.
 pub fn freeTagList(allocator: std.mem.Allocator, tags: *TagList) void {
     store_api.freeTagList(allocator, tags);
+}
+
+// Releases the owned tag strings returned by global tag queries.
+pub fn freeTagNameList(allocator: std.mem.Allocator, tags: *TagNameList) void {
+    store_api.freeStringList(allocator, tags);
 }
 
 /// Adds tags to a commit.
@@ -35,4 +48,29 @@ pub fn rm(
 ) !void {
     try store_api.ensureStoreVersion(allocator, omohi_dir);
     try store_api.tagRemove(allocator, omohi_dir, commit_id, tag_names);
+}
+
+test "listAll returns known tag names sorted ascending" {
+    var tmp = std.testing.tmpDir(.{});
+    defer tmp.cleanup();
+
+    const allocator = std.testing.allocator;
+    var omohi_dir = try tmp.dir.makeOpenPath(".omohi", .{ .iterate = true, .access_sub_paths = true });
+    defer omohi_dir.close();
+
+    try store_api.initializeVersionForFirstTrack(allocator, omohi_dir);
+    const tracked_path = try test_support.addTestFileForCommit(tmp.dir, allocator, "a.txt", "one");
+    defer allocator.free(tracked_path);
+
+    _ = try store_api.track(allocator, omohi_dir, tracked_path);
+    try store_api.add(allocator, omohi_dir, tracked_path);
+    const commit_id = try store_api.commit(allocator, omohi_dir, "first", false);
+    try store_api.tagAdd(allocator, omohi_dir, commit_id.asSlice(), &.{ "release", "alpha" });
+
+    var tags = try listAll(allocator, omohi_dir);
+    defer freeTagNameList(allocator, &tags);
+
+    try std.testing.expectEqual(@as(usize, 2), tags.items.len);
+    try std.testing.expectEqualStrings("alpha", tags.items[0]);
+    try std.testing.expectEqualStrings("release", tags.items[1]);
 }
